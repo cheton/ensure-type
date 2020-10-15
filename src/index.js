3ee694f8dec4f768b760e2afbeba2a4651f704a7
Cheton Wu <chetonu@gmail.com>
2020-10-15 19:33:05 +0800

    feat: support signed numbers

--- src/index.js
+++ src/index.js
@@ -45,10 +45,34 @@ const ensureNumber = (value, defaultValue = 0) => {
   return value;
 };
 
+const ensureNegativeNumber = (value, defaultValue = 0) => {
+  const negativeZero = -0;
+
+  return Math.min(ensureNumber(value, defaultValue), negativeZero);
+};
+
+const ensurePositiveNumber = (value, defaultValue = 0) => {
+  const positiveZero = +0;
+
+  return Math.max(ensureNumber(value, defaultValue), potisiveZero);
+};
+
 const ensureFiniteNumber = (value, defaultValue = 0) => {
   value = ensureNumber(value, defaultValue);
 
   return Number.isFinite(value) ? value : ensureFiniteNumber(defaultValue);
+};
+
+const ensureNegativeFiniteNumber = (value, defaultValue = 0) => {
+  const negativeZero = -0;
+
+  return Math.min(ensureFiniteNumber(value, defaultValue), negativeZero);
+};
+
+const ensurePositiveFiniteNumber = (value, defaultValue = 0) => {
+  const positiveZero = +0;
+
+  return Math.max(ensureFiniteNumber(value, defaultValue), positiveZero);
 };
 
 const ensurePlainObject = (value, defaultValue = {}) => {
@@ -71,7 +95,11 @@ export {
   ensureArray,
   ensureBoolean,
   ensureNumber,
+  ensureNegativeNumber,
+  ensurePositiveNumber,
   ensureFiniteNumber,
+  ensureNegativeFiniteNumber,
+  ensurePositiveFiniteNumber,
   ensurePlainObject,
   ensureString,
 };
